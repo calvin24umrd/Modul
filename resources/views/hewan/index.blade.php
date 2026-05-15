a451fa1e2fe97138b900c5f49f5336b11cecca3e
calvin24umrd <calvinumardi24@gmail.com>
2026-05-16 04:06:43 +0700

    Delete resources directory

--- resources/views/hewan/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-
-<div class="d-flex justify-content-between mb-3">
-    <h2>Data Hewan</h2>
-
-    @if(Auth::user()->role == 'admin')
-    <a href="{{ route('h
